29d1a0342e04a2c9ec3b95391d0031b4ef9c832e
matteo <matteo.cozzolino1@studenti.unimi.it>
2022-11-30 16:54:42 +0100

    queries part1

--- actions/actions.py
+++ actions/actions.py
@@ -1,6 +1,3 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-
 from typing import Any, Text, Dict, List
 from datetime import datetime, timedelta
 from rasa_sdk import Action, Tracker
@@ -12,7 +9,17 @@ DBUSERNAME = "chatbot"
 DBPW = "chatbotDB"
 DBADDRESS = "35.232.172.36"
 DBNAME = "h_farm"
+username = "user"   #TODO variabili temporanee con i dati dell'utente da dover estrarre dalla sessione in corso
+userEmail = "user@mail.com"
 
+#TEMP: ID dal DB per fare INSERT e testare le query
+#userid = 3 
+#courseid= 10,11,12 
+#enrolid= 25,28,31
+
+#TODO action per le lezioni
+
+#La classe gestisce la connessione al DB
 class DBConnectionHandler():
 
     def connect(self):
@@ -28,6 +35,20 @@ class DBConnectionHandler():
 
         return
 
+class Courses():
+
+    def getList(self):
+        connection = DBConnectionHandler.connect(self)
+        cursor = connection.cursor()
+
+        #Query per estrarre i corsi a cui lo studente è iscritto
+        cursor.execute ("SELECT shortname FROM mdl_course WHERE mdl_course.id IN (SELECT mdl_enrol.courseid FROM mdl_user_enrolments INNER JOIN mdl_enrol ON mdl_user_enrolments.enrolid = mdl_enrol.id WHERE mdl_user_enrolments.userid = (SELECT mdl_user.id FROM mdl_user WHERE mdl_user.username = %s))", (username, ))
+        courseName = cursor.fetchall()
+
+        DBConnectionHandler.closeConnection(self, connection)
+
+        return courseName
+
 class ActionGetCoursesList(Action):
 
     def name(self) -> Text:
@@ -37,31 +58,52 @@ class ActionGetCoursesList(Action):
         tracker: Tracker,
         domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        userFirstname = "user"
-        userEmail = "user@mail.com"
-
+        courseName = Courses.getList(self)
+    
         connection = DBConnectionHandler.connect(self)
         cursor = connection.cursor()
 
-        cursor.execute ("SELECT shortname FROM mdl_course")  #TODO da completare
-	    
-        userInfo = cursor.fetchall()
+        #Query per estrarre i corsi che lo studente ha già completato
+        cursor.execute("SELECT shortname FROM mdl_course WHERE mdl_course.id IN (SELECT mdl_course_completions.course FROM mdl_course_completions WHERE mdl_course_completions.userid = (SELECT mdl_user.id FROM mdl_user WHERE mdl_user.username = %s))", (username, ))
+        coursesCompleted = cursor.fetchall()
 
         DBConnectionHandler.closeConnection(self, connection)
-        
         buttons=[]
-        for i in userInfo:
-            tuple_to_str = "".join(i)
+
+        #I corsi già completati sono eliminati dalla lista dei corsi che lo studente può seguire
+        if len(coursesCompleted) > 0:
+            for j in coursesCompleted:
+                if courseName.count(j) > 0:
+                    courseName.remove(j)
+        for i in courseName:                
+            tuple_to_str = "".join(i)        
             fill_slot = '{"course" : "' + tuple_to_str + '"}'
-            buttons.append({"title": tuple_to_str, "payload" : f'/course_selected{fill_slot}'})
-        
+            buttons.append({"title": tuple_to_str, "payload" : f'/course_selected{fill_slot}'}) #TODO query per estrarre il tempo rimasto per finire un corso
+   
         dispatcher.utter_message(text = "Che corsi vorresti seguire?", buttons = buttons)
 
         return []
 
+class ActionGetCoursesEnrolled(Action):
 
-#TODO action per lessons_list
+     def name(self) -> Text:
+        return "action_get_courses_enrolled"
 
+     def run(self, dispatcher: CollectingDispatcher,
+             tracker: Tracker,
+             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        courseName = Courses.getList(self)
+    
+        buttons=[]
+        for i in courseName:                
+            tuple_to_str = "".join(i)        
+            fill_slot = '{"course" : "' + tuple_to_str + '"}'
+            buttons.append({"title": tuple_to_str, "payload" : f'/course_selected{fill_slot}'})
+   
+        dispatcher.utter_message(text = "Di quale corso vuoi più informazioni?", buttons = buttons)
+
+        return []
 
 class ActionGetCourseInfo(Action):
 
@@ -72,7 +114,17 @@ class ActionGetCourseInfo(Action):
              tracker: Tracker,
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text= 'info del corso ' + tracker.get_slot('course')) #TODO info da estrarre da db, creare buttons con title = nome corso da DB
+        connection = DBConnectionHandler.connect(self)
+        cursor = connection.cursor(buffered = True)
+
+        courseName = tracker.get_slot('course')
+
+        cursor.execute ("SELECT fullname, shortname, summary, startdate, enddate FROM mdl_course WHERE mdl_course.shortname =  %s", (courseName, ))
+        course_info = cursor.fetchall()
+            
+        DBConnectionHandler.closeConnection(self, connection)
+        
+        dispatcher.utter_message(text= 'Ecco le informazioni del corso ' + tracker.get_slot('course') + " " + " ".join(map(str, course_info)))    #TODO da elaborare per essere comprensibile
 
         return []
 
@@ -86,18 +138,16 @@ class ActionGetLink(Action):
              domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
             connection = DBConnectionHandler.connect(self)
-
             cursor = connection.cursor(buffered = True)
 
             courseName = tracker.get_slot('course')
 
             cursor.execute ("SELECT externalurl FROM mdl_url WHERE mdl_url.course = (SELECT id FROM mdl_course WHERE shortname = %s)", (courseName, ))
-            
             course_link = cursor.fetchone()
             
             DBConnectionHandler.closeConnection(self, connection)
 
-            dispatcher.utter_message(text= 'Ecco il link al corso ' + tracker.get_slot('course') + " " + "".join(course_link))
+            dispatcher.utter_message(text= 'Ecco il link al corso ' + tracker.get_slot('course') + " " + "".join(course_link))  #TODO sostituire il link con "clicca qui" o simili
             
             return []
 
